9a767cd1bd46674fb57b65a84e29e7e25794c5d6
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-11-03 17:03:37 +0100

    Add data for Hero section

---
+++ src/data/heroData.js
@@ -0,0 +1,27 @@
+import hero1 from "../assets/HeroAssets/hero1.jpg"
+import hero2 from "../assets/HeroAssets/hero2.jpg"
+import hero3 from "../assets/HeroAssets/hero3.jpg"
+import hero4 from "../assets/HeroAssets/hero4.jpg"
+
+export const HeroData = [
+    {
+        title: "Lorem ipsum dolor sit amet",
+        content: "Duis eget massa nec dui laoreet faucibus a et nulla. Praesent pretium nulla orci, in volutpat diam ullamcorper sit amet.",
+        backgroundImage: hero1,
+    },
+    {
+        title: "Sed arcu tortor",
+        content: "Sed arcu tortor, sodales quis erat vitae, maximus pellentesque neque. In blandit magna sem. Ut eu purus at libero varius tincidunt ut quis enim.",
+        backgroundImage: hero2,
+    },
+    {
+        title: "Vestibulum quis lacus",
+        content: "Vestibulum quis lacus quis nibh mattis malesuada lobortis id arcu. Nunc in massa purus. Aenean semper fringilla dui, ut ultrices est lobortis ut.",
+        backgroundImage: hero3,
+    },
+    {
+        title: "Donec efficitur egestas",
+        content: "Donec efficitur egestas sapien pharetra porta. Etiam quis scelerisque eros. Nam sit amet pretium dui. Maecenas nunc mi, porta cursus viverra fringilla, mattis sed sem.",
+        backgroundImage: hero4,
+    },
+];
