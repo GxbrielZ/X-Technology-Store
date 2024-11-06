baea63a65efc746e8ee290f637adefdcfa52a73f
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-11-06 21:25:12 +0100

    Add data with brands names and images

---
+++ src/data/brandsData.js
@@ -0,0 +1,33 @@
+import samsung from "../assets/BrandsAssets/samsung.png";
+import asus from "../assets/BrandsAssets/asus.png";
+import apple from "../assets/BrandsAssets/apple.png";
+import amd from "../assets/BrandsAssets/amd.png";
+import acer from "../assets/BrandsAssets/acer.png";
+import hp from "../assets/BrandsAssets/hp.png";
+
+export const BrandsData = [
+    {
+        name: "Asus-Logo",
+        logo: asus
+    },
+    {
+        name: "Apple-Logo",
+        logo: apple
+    },
+    {
+        name: "Samsung-Logo",
+        logo: samsung
+    },
+    {
+        name: "Acer-Logo",
+        logo: acer
+    },
+    {
+        name: "Amd-Logo",
+        logo: amd
+    },
+    {
+        name: "Hp-Logo",
+        logo: hp
+    },
+];
