468caa256e0e6c4730ae3b804f25b60a8f815d6a
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-11-07 20:13:09 +0100

    Create data with images and text for Bestsellers component

---
+++ src/data/bestsellersData.js
@@ -0,0 +1,73 @@
+import aockeyboard from "../assets/BestsellersAssets/aockeyboard.webp";
+import chairmonkey from "../assets/BestsellersAssets/chairmonkey.webp";
+import everoptima from "../assets/BestsellersAssets/everoptima.webp";
+import gigabyte from "../assets/BestsellersAssets/gigabyte.webp";
+import headphones from "../assets/BestsellersAssets/headphones.webp";
+import huaweiwatch from "../assets/BestsellersAssets/huaweiwatch.webp";
+import logitechmouse from "../assets/BestsellersAssets/logitechmouse.webp";
+import motorola from "../assets/BestsellersAssets/motorola.webp";
+import msilaptop from "../assets/BestsellersAssets/msilaptop.webp";
+import samsungtv from "../assets/BestsellersAssets/samsungtv.webp";
+
+export const BestsellersData = [
+    {
+        title: "Motorola moto g24 8/128GB Matte Charcoal",
+        price: "499,00 zł",
+        banner: "",
+        image: motorola
+    },
+    {
+        title: "Samsung QE65Q77C 65' QLED 4K 120HZ Tizen TV HDMI 2.1",
+        price: "3999,00 zł",
+        banner: "Ostatnie Sztuki",
+        image: samsungtv
+    },
+    {
+        title: "AOC GK200 Gaming Rainbow LX",
+        price: "69,00 zł",
+        banner: "Promocja",
+        image: aockeyboard
+    },
+    {
+        title: "Huawei Watch GT 5 Active czarny 46mm",
+        price: "1099,00 zł",
+        banner: "",
+        image: huaweiwatch
+    },
+    {
+        title: "Ever Optima - 6 gniazd, 1,5m, czarna",
+        price: "56,99 zł",
+        banner: "Ostatnie Sztuki",
+        image: everoptima
+    },
+    {
+        title: "MSI Cyborg 15 i5-12450H/16GB/512 RTX4060 144Hz",
+        price: "3799,00 zł",
+        banner: "Ostatnie Sztuki",
+        image: msilaptop
+    },
+    {
+        title: "Silver Monkey Fotel biurowy SMO-650",
+        price: "450,00 zł",
+        banner: "",
+        image: chairmonkey
+    },
+    {
+        title: "Beyerdynamic DT770 Pro 80Ohm",
+        price: "549,00 zł",
+        banner: "",
+        image: headphones
+    },
+    {
+        title: "Gigabyte B650 EAGLE",
+        price: "619,99 zł",
+        banner: "Promocja",
+        image: gigabyte
+    },
+    {
+        title: "Logitech G102 LIGHTSYNC czarna",
+        price: "109,99 zł",
+        banner: "",
+        image: logitechmouse
+    },
+];
