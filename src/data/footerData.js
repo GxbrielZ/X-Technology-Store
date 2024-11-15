9738847d51bc7f0d18ffd2d77f0265280e7491fe
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-11-15 19:56:15 +0100

    Create footer data for better maintaining

---
+++ src/data/footerData.js
@@ -0,0 +1,43 @@
+import visa from '../assets/FooterAssets/visa.png';
+import ups from '../assets/FooterAssets/ups.png';
+import dhl from '../assets/FooterAssets/dhl.png';
+import fedex from '../assets/FooterAssets/fedex.png';
+import blik from '../assets/FooterAssets/blik.png';
+
+export const FooterData = [
+    {
+        image: visa,
+        title: "Visa"
+    },
+    {
+        image: ups,
+        title: "UPS"
+    },
+    {
+        image: dhl,
+        title: "DHL"
+    },
+    {
+        image: fedex,
+        title: "FedEx"
+    },
+    {
+        image: blik,
+        title: "Blik"
+    },
+];
+
+export const FooterSections = [
+    {
+        title: "Zamówienia",
+        items: ["Dostawa", "Raty", "Leasing", "Wynajem sprzętu", "Ubezpieczenia", "TaxFree", "Montaż", "Zwroty i reklamacje", "Najczęściej zadawane pytania"],
+    },
+    {
+        title: "Promocje",
+        items: ["Wyprzedaże", "Promocje specjalne", "Karty podarunkowe", "Poradniki", "Aktualności"],
+    },
+    {
+        title: "X-Technology",
+        items: ["O nas", "Regulamin", "Polityka prywatności", "Polityka cookies", "Newsletter", "Zakupy dla firm", "Współpraca marketingowa", "Kariera", "Kontakt"],
+    }
+];
