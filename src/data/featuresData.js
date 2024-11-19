eec01f6fed52c41e009590382b0633c585c31015
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-11-19 19:20:00 +0100

    Create data for Features component

---
+++ src/data/featuresData.js
@@ -0,0 +1,27 @@
+import delivery from "../assets/FeaturesAssets/delivery.png";
+import payment from "../assets/FeaturesAssets/payment.png";
+import support from "../assets/FeaturesAssets/support.png";
+
+export const FeaturesData = [
+    {
+        id: 1,
+        title: "Szybka dostawa",
+        description: "Szybka i niezawodna dostawa na terenie całej Polski",
+        buttonLabel: "Sprawdź formy dostawy",
+        image: delivery
+    },
+    {
+        id: 2,
+        title: "Bezpieczna płatność",
+        description: "W pełni sprawdzony system płatności zapewniający bezpieczeństwo",
+        buttonLabel: "Sprawdź formy płatności",
+        image: payment
+    },
+    {
+        id: 3,
+        title: "Wsparcie online",
+        description: "Nasi specjaliści są gotowi pomóc z każdym problemem",
+        buttonLabel: "Uzyskaj wsparcie",
+        image: support
+    },
+];
