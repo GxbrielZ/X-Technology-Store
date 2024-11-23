876a9ba0c674b0dc7fe08decbfd4047608102473
Gabriel Zalewski <gabrielz2702@gmail.com>
2024-11-23 11:06:30 +0100

    Add data for Guides component

---
+++ src/data/guidesData.js
@@ -0,0 +1,51 @@
+import guide1 from '../assets/GuidesAssets/guide1.jpg';
+import guide2 from '../assets/GuidesAssets/guide2.jpg';
+import guide3 from '../assets/GuidesAssets/guide3.jpg';
+import guide4 from '../assets/GuidesAssets/guide4.jpg';
+import guide5 from '../assets/GuidesAssets/guide5.jpg';
+import guide6 from '../assets/GuidesAssets/guide6.jpg';
+import guide7 from '../assets/GuidesAssets/guide7.jpg';
+import guide8 from '../assets/GuidesAssets/guide8.jpg';
+
+export const GuidesData = [
+    {
+        image: guide1,
+        title: "Poradnik 1",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide2,
+        title: "Poradnik 2",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide3,
+        title: "Poradnik 3",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide4,
+        title: "Poradnik 4",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide5,
+        title: "Poradnik 5",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide6,
+        title: "Poradnik 6",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide7,
+        title: "Poradnik 7",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    },
+    {
+        image: guide8,
+        title: "Poradnik 8",
+        badge: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent pulvinar metus massa, blandit auctor nisl pulvinar nec."
+    }
+];
